c7f585f670a91f4a9018934472506c60fa118362
freedom.yi <851989962freedom.yi@gmail.com>
2019-12-16 22:06:52 +0800

    refactor: 测试 合并 分支问题

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
       <span>测试提交到 eslint 分支</span>
       <span>测试合并分支 到 master</span>
       <span>再次测试 新建分支</span>
+      <p>测试 合并 分支问题</p>
       <Home />
     </div>
   )
